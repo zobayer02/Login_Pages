7c0cf159884c64141d837fb7bef84c99c3b7d132
A.S.M. ZOBAYER <asmz.021084@gmail.com>
2025-01-10 23:59:24 +0600

    Add files via upload

---
+++ demo_1/login page/login.js
@@ -0,0 +1,12 @@
+
+const togglePassword = document.getElementById('togglePassword');
+const passwordInput = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+
+  const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordInput.setAttribute('type', type);
+
+
+  this.classList.toggle('fa-eye-slash');
+});
